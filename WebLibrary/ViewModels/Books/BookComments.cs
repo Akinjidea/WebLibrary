7def7227080099c553de946cb029208e6cd0889a
Maksim N <delsinrove48@gmail.com>
2021-02-22 20:10:10 +0300

    You can send your comments now!

---
+++ WebLibrary/ViewModels/Books/BookComments.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebLibrary.Models.Books;
+
+namespace WebLibrary.ViewModels.Books
+{
+    public class BookComments
+    {
+        public Book Book { get; set; }
+        public List<Comment> Comments { get; set; }
+        public Comment NewComment { get; set; }
+    }
+}
